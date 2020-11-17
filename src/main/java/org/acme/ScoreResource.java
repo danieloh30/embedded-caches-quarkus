24762ce132921c799f6ca8f79306207ef9b0be26
danieloh30 <doh@redhat.com>
2020-11-16 22:47:51 -0500

    Update packages

--- src/main/java/org/acme/ScoreResource.java
+++ src/main/java/org/acme/ScoreResource.java
@@ -3,15 +3,22 @@ package org.acme;
 import java.util.List;
 
 import javax.inject.Inject;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.OPTIONS;
+import javax.ws.rs.PATCH;
+import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.transaction.Transactional;
 import javax.validation.Valid;
-import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
 
 @Produces(MediaType.APPLICATION_JSON)
